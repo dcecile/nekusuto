083b71173866af1ab3b7cbf125448b11b86cfee4
Dan Cecile <dancecile@gmail.com>
2016-08-28 17:03:02 -0400

    Improve logo
    Also create a favicon version, simplify styling (no more pseudo before),
    and fix the Google Fonts URL.
    
    Progress on #11

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -45,6 +45,7 @@ var webpackConfig = merge(baseWebpackConfig, {
         ? 'index.html'
         : config.build.index,
       template: 'index.html',
+      favicon: 'src/assets/favicon.png',
       inject: true,
       minify: {
         removeComments: true,
